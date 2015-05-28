1a1d1ed50f849eeec2b85c931028672e44718ac8
ericyx <eric_yangxin@163.com>
2015-05-28 11:11:58 +0800

    will build http and framework

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,19 @@
-#include <iostream>       // std::cout
-#include <thread>         // std::thread
-#include <test_framework.hpp>       // std::cout
+#include <iostream>
+#include <thread>
+#include <mesos-microhttpd-framework/test_framework.hpp>
+#include <mesos-microhttpd-framework/test_microhttpd.hpp>
 
 int main(int argc, char** argv)
 {
+	int port = 8888;
+
 	std::thread mesosThread(testFramework, argc, argv);
-	//std::thread httpThread(startHttp, 0);
+	std::thread httpThread(testMicrohttpd, port);
 	std::cout << "main, mesos and http now execute concurrently...\n";
 
 	// synchronize threads:
 	mesosThread.join();                // pauses until first finishes
-	//httpThread.join();               // pauses until second finishes
+	httpThread.join();               // pauses until second finishes
 
 	std::cout << "Thread completed.\n";
 	std::getchar();
